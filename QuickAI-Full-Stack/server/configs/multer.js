70f92e51bf0d423cbb490589c9f3b1b9b176d1d2
Sayan Raha <109590974+sayanraha@users.noreply.github.com>
2025-11-13 13:30:38 +0530

    Delete QuickAI-Full-Stack directory

--- QuickAI-Full-Stack/server/configs/multer.js
+++
@@ -1,5 +0,0 @@
-import multer from "multer";
-
-const storage = multer.diskStorage({});// No need to specify destination and filename, multer will handle it by default
-
-export const upload = multer({storage})// Exporting the multer instance to be used in routes
